4b30b63a744f9f639218500694c19265d784609a
Shreyansh Raj <shreyansh.raj@tothenew.com>
2021-10-19 20:33:37 +0530

    print and Take binary search tree from user. ~ BST

--- BST/BinaryTreeUse.cpp
+++ BST/BinaryTreeUse.cpp
@@ -18,6 +18,47 @@ using namespace std;
 */
 
 
+void printTree(BinaryTreeNode<int>* root) {
+	if (root==NULL) {
+		return;
+	}
+
+	cout<<root->data<<" : ";
+
+	if (root->left != NULL) {
+		cout<<"L"<<root->left->data<<" ";
+	}
+
+	if (root->right != NULL) {
+		cout<<"R"<<root->right->data;
+	}
+
+	cout<<endl;
+	printTree(root->left);
+	printTree(root->right);
+}
+
+
+//1 2 -1 -1 3 -1 -1
+//1 2 4 -1 -1 5 -1 -1 3 -1 6 7 -1 -1 -1
+BinaryTreeNode<int>* takeTree() {
+	int rootData;
+
+	// cout<<"Enter Data : "<<endl;
+	cin>>rootData;
+
+	if (rootData == -1) {
+		return NULL;
+	}
+
+	BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+
+	root->left = takeTree();
+	root->right = takeTree();
+
+	return root;
+}
+
 
 int main() 
 {
@@ -26,11 +67,17 @@ int main()
     	freopen("output.txt", "w", stdout);
 	#endif
 
+    /*
     BinaryTreeNode<int>* root = new BinaryTreeNode<int>(1);
     BinaryTreeNode<int>* node1 = new BinaryTreeNode<int>(2);
     BinaryTreeNode<int>* node2 = new BinaryTreeNode<int>(3);
 
     root->left = node1;
     root->right = node2;
+    */
+
+
+    BinaryTreeNode<int>* root = takeTree();
+    printTree(root);
 
 }
